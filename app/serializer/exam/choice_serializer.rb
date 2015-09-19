e38d4726c501fd4836ba0286bf4b72cb2e995f1e
Majorvin Tan <majorvin.tan@mosaic.com>
2015-09-19 10:36:46 -0400

    Display correct answer when the exam is completed

--- app/serializer/exam/choice_serializer.rb
+++ app/serializer/exam/choice_serializer.rb
@@ -1,3 +1,9 @@
 class Exam::ChoiceSerializer < ActiveModel::Serializer
-  attributes :id, :text
+  attributes :id, :text, :answer
+
+  def answer
+    question = object.question
+    list = question.list
+    return object.answer if list.completed?
+  end
 end
